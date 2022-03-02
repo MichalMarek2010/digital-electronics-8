73854d05c2ca766bcae990d6e458bca2a3320e79
20David2 <99254001+20David2@users.noreply.github.com>
2022-03-02 14:19:24 +0100

    Edit

--- labs/03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
+++ labs/03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -47,7 +47,7 @@ architecture Behavioral of mux_3bit_4to1 is
 begin
 
 with sel_i select
-    f_o <= a_i when "00",  -- If sel_i = "000" then f_o = a_i
+    f_o <= a_i when "00",  -- If sel_i = "00" then f_o = a_i
            b_i when "01",
            c_i when "10",
            d_i when others; -- All other combinations
