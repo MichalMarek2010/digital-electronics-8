b0ff5f86596b0792a80ced5f88014b58edafd5b2
Martinececicek <80918583+Martinececicek@users.noreply.github.com>
2022-05-04 09:25:00 +0200

    Update alphabet_to_code.vhd

--- project/20220503/Simulation/Simulation.srcs/sources_1/imports/sources_1/imports/new/alphabet_to_code.vhd
+++ project/20220503/Simulation/Simulation.srcs/sources_1/imports/sources_1/imports/new/alphabet_to_code.vhd
@@ -47,7 +47,40 @@ entity alphabet_to_code is
     Port (
         text_i : in string (base_text_length_i downto 1);
         text_length_i : integer;
-        code_1_o : out std_logic_vector(5 downto 0)
+        code_1_o : out std_logic_vector(5 downto 0);
+        code_2_o : out std_logic_vector(5 downto 0);
+        code_3_o : out std_logic_vector(5 downto 0);
+        code_4_o : out std_logic_vector(5 downto 0);
+        code_5_o : out std_logic_vector(5 downto 0);
+        code_6_o : out std_logic_vector(5 downto 0);
+        code_7_o : out std_logic_vector(5 downto 0);
+        code_8_o : out std_logic_vector(5 downto 0);
+        code_9_o : out std_logic_vector(5 downto 0);
+        code_10_o : out std_logic_vector(5 downto 0);
+        code_11_o : out std_logic_vector(5 downto 0);
+        code_12_o : out std_logic_vector(5 downto 0);
+        code_13_o : out std_logic_vector(5 downto 0);
+        code_14_o : out std_logic_vector(5 downto 0);
+        code_15_o : out std_logic_vector(5 downto 0);
+        code_16_o : out std_logic_vector(5 downto 0);
+        code_17_o : out std_logic_vector(5 downto 0);
+        code_18_o : out std_logic_vector(5 downto 0);
+        code_19_o : out std_logic_vector(5 downto 0);
+        code_20_o : out std_logic_vector(5 downto 0);
+        code_21_o : out std_logic_vector(5 downto 0);
+        code_22_o : out std_logic_vector(5 downto 0);
+        code_23_o : out std_logic_vector(5 downto 0);
+        code_24_o : out std_logic_vector(5 downto 0);
+        code_25_o : out std_logic_vector(5 downto 0);
+        code_26_o : out std_logic_vector(5 downto 0);
+        code_27_o : out std_logic_vector(5 downto 0);
+        code_28_o : out std_logic_vector(5 downto 0);
+        code_29_o : out std_logic_vector(5 downto 0);
+        code_30_o : out std_logic_vector(5 downto 0);
+        code_31_o : out std_logic_vector(5 downto 0);
+        code_32_o : out std_logic_vector(5 downto 0);
+        code_33_o : out std_logic_vector(5 downto 0);
+        
         --
     );
 end alphabet_to_code;
@@ -70,11 +103,39 @@ begin
             char <= text_i(I);
             code(I) <= code_char;
         end loop;        
-        
-        code_1_o <= code(1);
-        
-            
-    end process p_mux;
+                          code_1_o <= code(1);
+                          code_2_o <= code(2);
+                          code_3_o <= code(3);
+                          code_4_o <= code(4);
+                          code_5_o <= code(5);
+                          code_6_o <= code(6);
+                          code_7_o <= code(7);
+                          code_8_o <= code(8);
+                          code_9_o <= code(9);
+                          code_10_o <= code(10);
+                          code_11_o <= code(11);
+                          code_12_o <= code(12);
+                          code_13_o <= code(13);
+                          code_14_o <= code(14);
+                          code_15_o <= code(15);
+                          code_16_o <= code(16);
+                          code_17_o <= code(17);
+                          code_18_o <= code(18);
+                          code_19_o <= code(19);
+                          code_20_o <= code(20);
+                          code_21_o <= code(21);
+                          code_22_o <= code(22);
+                          code_23_o <= code(23);
+                          code_24_o <= code(24);
+                          code_25_o <= code(25);
+                          code_26_o <= code(26);
+                          code_27_o <= code(27);
+                          code_28_o <= code(28);
+                          code_29_o <= code(29);
+                          code_30_o <= code(30);
+                          code_31_o <= code(31);
+                          code_32_o <= code(32);
+      end process p_mux;
 
     --------------------------------------------------------
     -- Instance (copy) of char_to_code entity
