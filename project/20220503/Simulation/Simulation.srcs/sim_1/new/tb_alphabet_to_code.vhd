f6767d6ec5d47e8397cfbe3789148b451e877bb1
20David2 <99254001+20David2@users.noreply.github.com>
2022-05-04 09:21:19 +0200

    Update

--- project/20220503/Simulation/Simulation.srcs/sim_1/new/tb_alphabet_to_code.vhd
+++ project/20220503/Simulation/Simulation.srcs/sim_1/new/tb_alphabet_to_code.vhd
@@ -15,19 +15,13 @@ end entity tb_alphabet_to_code;
 architecture testbench of tb_alphabet_to_code is
 
     -- Local signals
-    constant base_text_length : integer := 64;
+    constant base_text_length : integer := 32;
     
-    signal s_text_i : string (64 downto 1);
+    signal s_text_i : string (32 downto 1);
     signal s_text_length_i : integer;
---    signal s_code_o : std_logic_vector(5 downto 0);
-    signal s_data0 : std_logic_vector(5 downto 0);
-    signal s_data1 : std_logic_vector(5 downto 0);
-    signal s_data2 : std_logic_vector(5 downto 0);
-    signal s_data3 : std_logic_vector(5 downto 0);
-    signal s_data4 : std_logic_vector(5 downto 0);
-    signal s_data5 : std_logic_vector(5 downto 0);
-    signal s_data6 : std_logic_vector(5 downto 0);
-    signal s_data7 : std_logic_vector(5 downto 0);
+
+    type TEST_ARRAY is array (natural range <>) of std_logic_vector; 
+    signal s_code_o : TEST_ARRAY(32 downto 1) (5 downto 0);
     
         
 begin
@@ -40,15 +34,7 @@ begin
         port map(
             text_i          => s_text_i,
             text_length_i   => s_text_length_i,
---            code_o          => s_code_o
-            data0_i => s_data0,
-            data1_i => s_data1,
-            data2_i => s_data2,
-            data3_i => s_data3,
-            data4_i => s_data4,
-            data5_i => s_data5,
-            data6_i => s_data6,
-            data7_i => s_data7
+            code_1_o        => s_code_o(1)
         );
 
     --------------------------------------------------------
